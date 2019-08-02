89a3833be446d9aa1a4a1caee3dea81f17d48698
lfrodriguez <lfrodriguez06@gmail.com>
2019-08-02 18:22:25 -0500

    improved form styling

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -2,16 +2,61 @@ import React from "react"
 import styled from "styled-components"
 
 const FormContainer = styled.div`
-  margin: 0;
+  display: grid;
+  grid-template-rows: auto;
+  border: 1px solid red;
 `
 
 const StyledForm = styled.form`
-  display: grid;
-  margin: 0;
+  margin: 1.7em;
 `
 
 const StyledFormInput = styled.input`
-  margin: 1.5em;
+  margin: 0 0 0.67em 0;
+  padding: 0.3em;
+  background: #684a83;
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  width: 100%;
+  height: 45px;
+  box-shadow: 0 1px 7px rgba(0, 0, 0, 0.5);
+  border-radius: 4px;
+  &:focus {
+    background: 1px #59c3c3;
+  }
+`
+const DivForm = styled.form`
+  margin: 0 0.67em 0em 0em;
+  display: grid;
+`
+
+const TextArea = styled.textarea`
+  margin: 0 0 0.67em 0;
+  padding: 0.3em;
+  background: #684a83;
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  box-shadow: 0 1px 7px rgba(0, 0, 0, 0.5);
+  border-radius: 4px;
+  width: 100%;
+  &:focus {
+    background: 1px #59c3c3;
+  }
+`
+const FormButton = styled.button`
+  display: grid;
+  cursor: default;
+  margin: 0 0 1em 0;
+  background-color: #684a83;
+  justify-self: center;
+  color: white;
+  width: 30%;
+  font-size: 0.9rem;
+  padding: 0.6rem;
+  font-weight: 500;
+  border-radius: 24px;
 `
 
 export default function Forms() {
@@ -23,20 +68,20 @@ export default function Forms() {
           type="text"
           name="name"
           id="name"
-          placeholder="your name"
+          placeholder="What's your name, friend?"
           required
         />
         <StyledFormInput
           type="text"
           name="email"
           id="email"
-          placeholder="your email"
+          placeholder="And your email?"
           required
         />
-        <textarea
+        <TextArea
           name="message"
           id="message"
-          placeholder="your message"
+          placeholder="How can I help? You can use this space to share a brief description of your problem."
           rows="4"
           required
         />
