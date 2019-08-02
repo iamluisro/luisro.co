0ba19e089057ab1d477f6b88563f652f1990d594
lfrodriguez <lfrodriguez06@gmail.com>
2019-08-01 22:51:00 -0500

    throwing garbage out there

---
+++ src/components/Intro.js
@@ -0,0 +1,36 @@
+import React from "react"
+import styled from "styled-components"
+
+const IntroContainer = styled.div`
+  display: grid;
+  padding: 2em 1em 2em 1em;
+  background-color: #684a83;
+`
+const IntroHead = styled.div`
+  color: #ffff;
+  font-size: 2em;
+  margin: 0 0 0.67em 0;
+  text-align: center;
+`
+
+const IntroTxt = styled.p`
+  margin: 0 0 0 0;
+  color: #ffff;
+  font-size: 1.5em;
+  text-align: justify;
+`
+
+export default function Intro() {
+  return (
+    <div className="Intro">
+      <IntroContainer>
+        <IntroHead>Thanks for being here!</IntroHead>
+        <IntroTxt>
+          My goal is to solve a customer's needs and provide the best customer
+          experience to ensure company growth, employee satisfaction and
+          customer delight.
+        </IntroTxt>
+      </IntroContainer>
+    </div>
+  )
+}
---
+++ src/pages/thanks.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Thanks = () => (
+  <>
+    <h1>Thanks!</h1>
+    <p>We'll get in touch soon!</p>
+  </>
+)
+
+export default Thanks
