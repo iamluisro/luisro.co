2f589e24ff6dd5c839b58445e33c68d7c48b3630
lfrodriguez <lfrodriguez06@gmail.com>
2019-08-02 01:18:48 -0500

    removed form from index and added it to forms.js

--- public/component---src-pages-past-work-js-2105de72809f1b222398.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{216:function(n,e,t){"use strict";t.r(e),t.d(e,"default",function(){return s});var i=t(0),a=t.n(i),o=t(19).d.p.withConfig({displayName:"pastWork__AboutContainer",componentId:"sc-9sotni-0"})(["margin:0 0 0 0;padding-top:2em;font-size:2em;"]);function s(){return a.a.createElement("div",null,a.a.createElement(o,null,"This is my past work"),a.a.createElement("p",null,"This is my past work"))}}}]);
-//# sourceMappingURL=component---src-pages-past-work-js-2105de72809f1b222398.js.map
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,31 +21,6 @@ const IndexPage = () => {
         <ThinkAbout />
         <PastWork />
         <HowiHelp />
-        <form method="post" action="#" data-netlify="true" name="contact">
-          <input type="hidden" name="form-name" value="contact" />
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="your name"
-            required
-          />
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="your email"
-            required
-          />
-          <textarea
-            name="message"
-            id="message"
-            placeholder="your message"
-            rows="4"
-            required
-          />
-          <input type="submit" value="Send Message" />
-        </form>
       </div>
     </>
   )
