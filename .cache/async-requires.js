cef5ecb3263c7eece615add19a01179f5d07240b
lfrodriguez <lfrodriguez06@gmail.com>
2019-08-01 21:37:35 -0500

    cleaned gatsby cache and public

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -9,6 +9,7 @@ exports.components = {
   "component---src-pages-page-2-jsx": () => import("/Users/luis/coding_proyects/luisro.co/src/pages/page-2.jsx" /* webpackChunkName: "component---src-pages-page-2-jsx" */),
   "component---src-pages-past-work-js": () => import("/Users/luis/coding_proyects/luisro.co/src/pages/pastWork.js" /* webpackChunkName: "component---src-pages-past-work-js" */),
   "component---src-pages-resume-jsx": () => import("/Users/luis/coding_proyects/luisro.co/src/pages/resume.jsx" /* webpackChunkName: "component---src-pages-resume-jsx" */),
+  "component---src-pages-thanks-js": () => import("/Users/luis/coding_proyects/luisro.co/src/pages/thanks.js" /* webpackChunkName: "component---src-pages-thanks-js" */),
   "component---src-pages-think-about-js": () => import("/Users/luis/coding_proyects/luisro.co/src/pages/thinkAbout.js" /* webpackChunkName: "component---src-pages-think-about-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -12,6 +12,7 @@ exports.components = {
   "component---src-pages-page-2-jsx": hot(preferDefault(require("/Users/luis/coding_proyects/luisro.co/src/pages/page-2.jsx"))),
   "component---src-pages-past-work-js": hot(preferDefault(require("/Users/luis/coding_proyects/luisro.co/src/pages/pastWork.js"))),
   "component---src-pages-resume-jsx": hot(preferDefault(require("/Users/luis/coding_proyects/luisro.co/src/pages/resume.jsx"))),
+  "component---src-pages-thanks-js": hot(preferDefault(require("/Users/luis/coding_proyects/luisro.co/src/pages/thanks.js"))),
   "component---src-pages-think-about-js": hot(preferDefault(require("/Users/luis/coding_proyects/luisro.co/src/pages/thinkAbout.js")))
 }
 
